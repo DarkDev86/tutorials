3da95096168d6f3d34da1e4b806e64b8588cad44
SE <106319675+DarkDev86@users.noreply.github.com>
2023-04-03 02:08:29 +0200

    console.log

---
+++ javascript.js
@@ -0,0 +1,7 @@
+//console.log();
+console.log("Hello World");
+console.log("Hello".length);//this is a string property
+console.log("Hello".toLowerCase());//this is a string method and therefore brackets have to be used
+console.log("Hello".startsWith("F"));
+console.log("Hello".charAt(1));
+console.log("Hello"[1]);
