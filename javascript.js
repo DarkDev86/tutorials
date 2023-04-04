b1e11c21080dc92442089e1c64777725045ab853
SE <106319675+DarkDev86@users.noreply.github.com>
2023-04-04 04:56:58 +0200

    increment and decrement

--- javascript.js
+++ javascript.js
@@ -5,3 +5,10 @@ console.log("Hello".toLowerCase());//this is a string method and therefore brack
 console.log("Hello".startsWith("F"));
 console.log("Hello".charAt(1));
 console.log("Hello"[1]);
+
+let age1 = 37;
+let age2 = 37;
+++age1; //postfix increment
+age2++; //prefix increment 
+console.log("Age 1 is " + age1);
+console.log("Age 2 is " + age2);
